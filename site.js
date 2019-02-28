6a466cd0129bc408aa498b9fc3eddd6ccb92bfc4
glonngren <glonngren@hawk.iit.edu>
2019-02-28 13:08:22 -0600

    Removed Field
    Removed name field for simplicity

--- site.js
+++ site.js
@@ -12,13 +12,8 @@
     return validate(email,/^[^@\s]+@[^@\s]+$/g);
   }
   
-  function validate_name(value) { 
-    return validate(name,/^[A-Za-z]+$/);
-  }
-
   document.addEventListener('DOMContentLoaded',function(){
     // Select the necessary elements from the DOM
-    var signup_submit = document.querySelector('#name');
     var email_input = document.querySelector('#email');
     // Disable the submit button until we are reasonable sure
     // that we have a ten-digit phone number
@@ -27,7 +22,7 @@
     // Listen for keyup event ANYWHERE in the form
     signup_form.addEventListener('keyup',function(){
       // Check the likely validity of phone AND email
-      if (validate_email(email_input.value) & validate_name(signup_submit.value)) {
+      if (validate_email(email_input.value)) {
         // If valid, remove the disabled attribute on the submit button
         signup_submit.removeAttribute('disabled');
       } else {
